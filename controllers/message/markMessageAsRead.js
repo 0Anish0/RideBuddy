2d9c84830464c39371c12b903e5ee31fda43c190
0Anish0 <anishpandey4576@gmail.com>
2024-09-08 02:51:54 +0530

    update

--- controllers/message/markMessageAsRead.js
+++ controllers/message/markMessageAsRead.js
@@ -1,19 +1,49 @@
+// const Message = require('../../models/Message');
+
+// const markMessageAsRead = async (req, res) => {
+//     try {
+//         const { messageId } = req.params;
+
+//         const updatedMessage = await Message.findByIdAndUpdate(
+//             messageId,
+//             { isRead: true },
+//             { new: true }
+//         );
+
+//         res.json(updatedMessage);
+//     } catch (err) {
+//         res.status(500).send(err);
+//     }
+// };
+
+// module.exports = markMessageAsRead;
+
+
+
+
+
+
+
+
+
+
 const Message = require('../../models/Message');
 
-const markMessageAsRead = async (req, res) => {
+const markMessagesAsRead = async (req, res) => {
     try {
-        const { messageId } = req.params;
+        const { senderId, receiverId } = req.params;
 
-        const updatedMessage = await Message.findByIdAndUpdate(
-            messageId,
-            { isRead: true },
+        // Update all unread messages from sender to receiver
+        const updatedMessages = await Message.updateMany(
+            { sender: senderId, receiver: receiverId, isRead: false },
+            { $set: { isRead: true } }, // Set all unread messages to read
             { new: true }
         );
 
-        res.json(updatedMessage);
+        res.json({ success: true, updatedMessages });
     } catch (err) {
         res.status(500).send(err);
     }
 };
 
-module.exports = markMessageAsRead;
+module.exports = markMessagesAsRead;
