185c8738b7e7e26ca380e7fee3f74bea0f51f543
0Anish0 <anishpandey4576@gmail.com>
2024-09-10 12:55:07 +0530

    update

--- controllers/register/getProfile.js
+++ controllers/register/getProfile.js
@@ -1,15 +1,18 @@
+const User = require('../../models/User');
 const Profile = require('../../models/Profile');
 
 const getProfile = async (req, res) => {
     const { userId } = req.params;
 
     try {
-        const profile = await Profile.findOne({ userId });
-
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const profile = await Profile.findOne({ userId: user._id });
         if (!profile) {
             return res.status(404).json({ message: 'Profile not found' });
         }
-
         return res.status(200).json({ profile });
     } catch (error) {
         return res.status(500).json({ message: 'Error retrieving profile', error: error.message });
--- routes/register.js
+++ routes/register.js
@@ -14,7 +14,7 @@ const { getProfile } = require('../controllers/register/getProfile.js');
 router.post('/send-otp', sendOtp); // Route to send OTP
 router.post('/verify-otp', verifyOtp); // Route to verify OTP
 router.post('/create-profile', authenticateToken, createProfile); // Route to create profile
-router.post('/get-profile', authenticateToken, getProfile); // Route to get profile
+router.get('/get-profile/:userId', getProfile); // Route to get profile
 router.post('/update/:userId', authenticateToken, upload.fields([
     { name: 'profilePicture', maxCount: 1 },
     { name: 'images', maxCount: 5 }
